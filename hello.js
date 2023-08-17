7321c46c2ec0268a2ab47b8cd8f68024d389c7b4
Dhairyas14 <dhairyas14@gmail.com>
2023-08-17 19:09:33 +0530

    User Details info added to 1st program

--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
 console.log("Hello, World!");
+
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+r1.question('Please enter your name: ', (name) =>{
+    r1.question('Pleaser enter your email: ', (email)=>{
+        if (name.trim( ) === '' || email.trim() === ''){
+            console.log('Both name and email are mandatory fields.');
+        } else{
+            console.log('User details:');
+            console.log('Name:', name);
+            console.log('Email', email);
+        }
+        r1.close();
+    })
+})
